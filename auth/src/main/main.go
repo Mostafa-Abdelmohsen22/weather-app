814b07f0b4fdc48bbabab3beddf21a9dfc2b3181
Ahmed363-Essam <ae676430@gmail.com>
2025-01-08 01:39:57 +0200

    solve issue of run

--- auth/src/main/main.go
+++ auth/src/main/main.go
@@ -53,7 +53,7 @@ func main() {
 	router.GET("/", health)
 	router.POST("/users/:id", loginUser)
 	router.POST("/users", createUser)
-	router.Run(":8090")
+	router.Run(":8060")
 }
 
 type UserCreds struct {
